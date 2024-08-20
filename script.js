33c846bede02011c61c727ea152c35e0213281ae
jackinov957 <151686341+jackinov957@users.noreply.github.com>
2024-08-20 12:59:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-let dollarCount = 0;
-
-const skibidiImage = document.getElementById('skibidi-image');
-const dollarCountDisplay = document.getElementById('dollar-count');
-
-// Increment dollar count on image click
-skibidiImage.addEventListener('click', function() {
-    dollarCount++;
-    dollarCountDisplay.textContent = 'Dollars: ' + dollarCount;
+document.getElementById('skibidiImage').addEventListener('click', function() {
+    alert('You earned a dollar!');
 });
